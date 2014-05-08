49c2cffb44c17cccd40e79d676bde9eef49b7282
tomdepplito <tomdepplito@gmail.com>
2014-05-08 14:20:15 -0400

    Added rdoc for twitter_stream

--- app/models/twitter_stream.rb
+++ app/models/twitter_stream.rb
@@ -1,4 +1,10 @@
+# This class enables access to Twitter's streaming API
+
 class TwitterStream
+
+  # Create a new Twitter streaming client if one has not already
+  # been created.  It will be necessary to call this method for all
+  # interactions with Twitter's API.
   def self.client
     @client ||= Twitter::Streaming::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
@@ -8,7 +14,10 @@ class TwitterStream
     end
   end
 
-
+  # Listen to a random sampling of tweets and store the tweet
+  # in MongoDB if it is an original tweet that has been
+  # retweeted by other users.  This method is intended to run
+  # indefinitely.
   def self.listen_for_retweets
     client.sample do |object|
       case object
