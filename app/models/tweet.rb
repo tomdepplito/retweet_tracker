58f3cef5522c7bd82934fae4cbdd9d0d285921d5
tomdepplito <tomdepplito@gmail.com>
2014-05-07 22:48:21 -0400

    Completed full map reduce

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,41 +10,46 @@ class Tweet
   validates_numericality_of :retweet_count
 
   def self.most_retweeted(time_window, top_count)
-    map    = %Q{
-                 var key = this.twitter_id;
-                 var value = {
-                                twitterID: key,
-                                retweetCount: this.retweet_count
-                              };
-                 emit(key, value);
-               }
+    map     = %Q{
+                  var key = this.twitter_id;
+                  var value = {
+                                 twitterID: key,
+                                 retweetCount: this.retweet_count,
+                                 text: this.text
+                               };
+                  emit(key, value);
+                }
 
-    reduce = %Q{ function(key, values) {
-                   var reducedObject = {
-                                         twitterID: key,
-                                         retweetCount: 0
-                                        };
+    reduce  = %Q{ function(key, values) {
+                    var reducedObject = {
+                                          twitterID: key,
+                                          text: values[0].text,
+                                          retweetCount: 0
+                                         };
 
-                   var min = max = values[0].retweetCount;
+                    var min = max = values[0].retweetCount;
 
-                   values.forEach(function(value) {
-                     var currentCount = value.retweetCount;
+                    values.forEach(function(value) {
+                      var currentCount = value.retweetCount;
 
-                     if(currentCount > max) {
-                       max = currentCount;
-                     }
+                      if(currentCount > max) {
+                        max = currentCount;
+                      }
 
-                     if(currentCount < min) {
-                       min = currentCount;
-                     }
-                   });
+                      if(currentCount < min) {
+                        min = currentCount;
+                      }
+                    });
 
-                   reducedObject.retweetCount = max - min;
-                   return reducedObject;
-                 };
-               }
+                    reducedObject.retweetCount = max - min;
+                    return reducedObject;
+                  };
+                }
 
     Tweet.gte(read_time: Time.now - time_window).
-      map_reduce(map, reduce).out(inline: true)
+      map_reduce(map, reduce).out(inline: true).
+      sort_by do |a,b|
+        b.try([:value]).try([:retweetCount]) <=> a.try([:value]).try([:retweetCount])
+      end[0..top_count-1]
   end
 end
